c7a974850e62f4dbe08b9c89f55eeba169765c0a
felixgro <me@felixgrohs.dev>
2021-10-25 19:41:37 +0200

    refactor(Program): add Programm class in lib

--- cli.js
+++ cli.js
@@ -1,43 +1,41 @@
 #!/usr/bin/env node
-const ModuleExecutor = require('./core/ModuleExecutor');
-const createFile = require('./utils/createFile');
 const pkg = require('./package.json');
-const program = require('commander');
-const { join } = require('path');
+const Program = require('./lib/Program');
+const ModuleExecutor = require('./core/ModuleExecutor');
+const getAbsolutePath = require('./utils/getAbsolutePath');
+const createMissingModule = require('./utils/createMissingModule');
 
-program
-	.name(Object.keys(pkg.bin)[0])
-	.version(pkg.version)
+// initialize cli program using name and version as defined in package.json
+const program = new Program(Object.keys(pkg.bin)[0], pkg.version).argument(
+	'module',
+	'module file name for execution duration meassuring. if no file found, bora will create a template file with the provided name'
+);
 
-	.argument('<module>', 'Module for execution duration meassuring')
-
-	.option(
-		'-i, --iterations <number>',
-		'How often each method gets called for average execution duration',
-		1
-	)
-
-	.option(
-		'-p, --precision <number>',
-		'Amount of decimal places for resulting milliseconds',
-		0
-	);
-
-program.action(async (relModulePath, opts) => {
-	// get absolute module path..
-	const modulePath = join(process.cwd(), relModulePath);
-
-	// try to import argument module and execute all of it's default methods..
-	try {
-		if (opts.new) {
-			createFile(modulePath, '.js');
-		} else {
-			const executor = new ModuleExecutor(modulePath);
-			executor.run(opts.iterations, opts.precision);
-		}
-	} catch (err) {
-		console.log(err);
-	}
+program.options({
+	iterations: {
+		type: 'number',
+		description: 'amount of iterations for avarage execution time calculations',
+		default: 1,
+	},
+	precision: {
+		type: 'number',
+		description: 'amount of decimal places for resulting milliseconds',
+		default: 0,
+	},
 });
 
-program.parse(process.argv);
+program.execute(async (relModulePath, opts) => {
+	// path is relative to pwd of user, this method converts it to an absolute path
+	// as well as assigns the .js file extension if not provided.
+	const modulePath = getAbsolutePath(relModulePath, '.js');
+
+	// checks if given module and directories exist on the system. If so, this evaluates
+	// to false and the execution function continues. If the directory and/or file does not exists,
+	// it creates the necessary directory structor along with the testing file and terminates the process.
+	if (await createMissingModule(modulePath)) return;
+
+	// executes the specified module by spawing a worker thread and prints
+	// the resulting data afterwards.
+	const executor = new ModuleExecutor(modulePath);
+	executor.run(opts.iterations, opts.precision);
+});
