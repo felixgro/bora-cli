75d89992de8859fef7d1fcee1acb50cf1842e20b
felixgro <me@felixgrohs.dev>
2021-10-26 14:23:10 +0200

    refactor(add comments)

--- core/ExecutionWorker.js
+++ core/ExecutionWorker.js
@@ -1,39 +1,39 @@
 const { workerData, parentPort } = require('worker_threads');
-
-// utility functions
 const createTimer = require('../utils/createTimer');
 const roundDecimals = require('../utils/roundDecimals');
 
-// receive data passed to worker thread..
+// received data passed to worker thread..
 const { modulePath, iterations, precision } = workerData;
 
 import(modulePath).then((mod) => {
+	// all methods and variables defined exported from testing module.
 	const defaultExports = mod.default;
+	// results[methodName]: avarage execution duration in ms
 	const results = {};
 
-	const argument = defaultExports['setup']
-		? defaultExports['setup'].call({})
-		: null;
+	// argument(s) received from setup method's return value, if this value is not an
+	// array of multiple values, it will be converted to an array to enable the use
+	// of the spread operator later when each testing method get's called with all specified arguments.
+	let arguments = defaultExports['setup'] ? defaultExports['setup'].call({}) : null;
+	if (!(arguments instanceof Array)) arguments = [arguments];
 
 	for (const method in defaultExports) {
-		if (typeof defaultExports[method] !== 'function' || method === 'setup')
-			continue;
+		// check if current method is callable and exclude the previously executed setup method.
+		if (typeof defaultExports[method] !== 'function' || method === 'setup') continue;
 
 		const timer = createTimer();
 		let durationSum = 0;
 
 		for (let i = 0; i < parseInt(iterations); i++) {
 			timer.start();
-			defaultExports[method].call({}, argument);
+			defaultExports[method].call({}, ...arguments);
 			durationSum += timer.stop();
 		}
 
 		// calculate avarage execution time with specified precision and store it within results..
-		results[method] = roundDecimals(
-			durationSum / iterations,
-			parseInt(precision)
-		);
+		results[method] = roundDecimals(durationSum / iterations, parseInt(precision));
 	}
 
+	// send resulting data back to main thread
 	parentPort.postMessage(results);
 });
